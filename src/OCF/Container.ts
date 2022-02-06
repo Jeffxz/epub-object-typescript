7c39f3e2e6df79f1d62ba1612692d849b96dfe75
Zheng Xu <zhengxu7806@gmail.com>
2022-02-06 15:53:40 -0500

    chore: add try catch

--- src/OCF/Container.ts
+++ src/OCF/Container.ts
@@ -21,26 +21,30 @@ export default class Container {
 
   static loadFromXML(xmlString: string): Container | null {
     let container: Container | null = null
-    const document = new XML.XmlDocument(xmlString)
-    const rootfiles = document.childNamed('rootfiles')
-    if (rootfiles == null) {
-      console.error('Could not find rootfiles')
-      return container
-    }
-    const rootfile = rootfiles?.childrenNamed('rootfile')
-    if (rootfile.length == 0) {
-      console.error('Could not find rootfile inside rootfiles')
-      return container
-    }
-    const opfPathList: string[] = []
-    for (const node of rootfile) {
-      if (node?.attr['full-path']) {
-        opfPathList.push(node?.attr['full-path'])
+    try {
+      const document = new XML.XmlDocument(xmlString)
+      const rootfiles = document.childNamed('rootfiles')
+      if (rootfiles == null) {
+        console.error('Could not find rootfiles')
+        return container
       }
-    }
+      const rootfile = rootfiles?.childrenNamed('rootfile')
+      if (rootfile.length == 0) {
+        console.error('Could not find rootfile inside rootfiles')
+        return container
+      }
+      const opfPathList: string[] = []
+      for (const node of rootfile) {
+        if (node?.attr['full-path']) {
+          opfPathList.push(node?.attr['full-path'])
+        }
+      }
 
-    if (opfPathList) {
-      container = new Container(opfPathList)
+      if (opfPathList) {
+        container = new Container(opfPathList)
+      }
+    } catch (error) {
+      console.error((error as Error).message)
     }
     return container
   }
